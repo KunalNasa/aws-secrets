d2fe4117ddaa4843080132980bd6ced8b70da205
KunalNasa <kunalnasa.dev@gmail.com>
2025-04-17 23:24:49 +0530

    setup complete

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+dotenv.config();
+
+
+app.get('/', (req, res) => {
+  return res.json({
+    "name" : "Kunal",
+    "secret" : process.env.MY_SECRET,
+  });
+}
+);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
